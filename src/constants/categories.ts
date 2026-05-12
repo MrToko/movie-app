e20dda3d925954d2f9285e0f86e529ff8a29628a
TheTokoo <emirhan.tok@stu.fsm.edu.tr>
2026-05-12 13:27:30 +0300

    add infinite scroll to categories screen

---
+++ src/constants/categories.ts
@@ -0,0 +1,15 @@
+// kategoriler burada tanımlanır çünkü bu kategoriler birden fazla yerde kullanılabilir
+const CATEGORIES = [
+  { key: "popular", label: "Popular", query: "marvel" },
+  { key: "action", label: "Action", query: "action" },
+  { key: "adventure", label: "Adventure", query: "adventure" },
+  { key: "sci-fi", label: "Sci-Fi", query: "sci-fi" },
+  { key: "drama", label: "Drama", query: "drama" },
+  { key: "comedy", label: "Comedy", query: "comedy" },
+  { key: "romance", label: "Romance", query: "romance" },
+  { key: "thriller", label: "Thriller", query: "thriller" },
+  { key: "animation", label: "Animation", query: "animation" },
+  { key: "horror", label: "Horror", query: "horror" },
+];
+
+export default CATEGORIES;
