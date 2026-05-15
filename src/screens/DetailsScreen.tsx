07cc59d9abdeb39fcb565ac4ed163ab6a4b624f1
TheTokoo <emirhan.tok@stu.fsm.edu.tr>
2026-05-15 15:15:51 +0300

    refactor: move saved movies state to context
    Implemented global state management using Context API for saved movies and refactored screens to use shared state.

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -8,22 +8,26 @@ import {
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
-import { getMovieDetails, OmdbDetail } from "../api/omdb";
+import { getMovieDetails } from "../api/services/movie.service";
+import { OmdbDetail } from "../models/movie.model";
 import { SafeAreaView } from "react-native-safe-area-context";
 import colors from "../theme/colors";
 import { s, vs } from "react-native-size-matters";
 import MovieInfo from "../components/MovieInfo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MovieInfoRow from "../components/MovieInfoRow";
-import { isSaved, toggleSave } from "../storage/saved";
+import { useMovieContext } from "../context/MovieContext";
 
 const DetailsScreen = () => {
-  const { name, params } = useRoute<any>();
+  const { params } = useRoute<any>();
+
   const [movie, setMovie] = useState<OmdbDetail | null>(null);
-  const [saved, setSaved] = useState(false);
+
+  const { toggleSaveMovie, isMovieSaved } = useMovieContext();
 
   const fetchMovieDetail = async () => {
     const res = await getMovieDetails(params.movieImdbID);
+
     setMovie(res);
   };
 
@@ -31,32 +35,30 @@ const DetailsScreen = () => {
     fetchMovieDetail();
   }, [params.movieImdbID]);
 
-  useEffect(() => {
-    const checkSaved = async () => {
-      const savedStatus = await isSaved(params.movieImdbID);
-      setSaved(savedStatus);
-    };
-    checkSaved();
-  }, [params.movieImdbID]);
-
   const textCapitilaze = (text?: string) => {
     return text ? text.charAt(0).toUpperCase() + text.slice(1) : "";
   };
+
+  const saved = movie ? isMovieSaved(movie.imdbID) : false;
 
   return (
     <SafeAreaView style={styles.container} edges={[]}>
       <ScrollView contentContainerStyle={{ padding: s(12) }}>
         <Image style={styles.moviePoster} source={{ uri: movie?.Poster }} />
+
         <View style={{ marginTop: vs(12), gap: 10 }}>
           <Text style={styles.movieTitle}>{movie?.Title}</Text>
+
           <View style={styles.movieInfoContainer}>
             <MovieInfo label={movie?.Rated} />
             <MovieInfo label={movie?.Runtime} />
             <MovieInfo label={movie?.Year} />
             <MovieInfo label={textCapitilaze(movie?.Type)} />
           </View>
+
           <View style={styles.movieRatingContainer}>
             <Ionicons name="star" color={"#fbbf24"} size={s(14)} />
+
             <Text
               style={[
                 styles.movieRatingText,
@@ -65,6 +67,7 @@ const DetailsScreen = () => {
             >
               {movie?.imdbRating}
             </Text>
+
             <Text
               style={[
                 styles.movieRatingText,
@@ -74,6 +77,7 @@ const DetailsScreen = () => {
               ({movie?.imdbVotes})
             </Text>
           </View>
+
           <Text style={{ color: "#9ca3af" }}>{movie?.Genre}</Text>
         </View>
 
@@ -87,7 +91,14 @@ const DetailsScreen = () => {
           >
             Overview
           </Text>
-          <Text style={{ color: "#cbd5e1", marginTop: vs(2), lineHeight: 18 }}>
+
+          <Text
+            style={{
+              color: "#cbd5e1",
+              marginTop: vs(2),
+              lineHeight: 18,
+            }}
+          >
             {movie?.Plot}
           </Text>
         </View>
@@ -102,14 +113,23 @@ const DetailsScreen = () => {
           >
             Info
           </Text>
+
           <MovieInfoRow label="Director" value={movie?.Director} />
+
           <MovieInfoRow label="Writer" value={movie?.Writer} />
+
           <MovieInfoRow label="Actors" value={movie?.Actors} />
+
           <MovieInfoRow label="Language" value={movie?.Language} />
+
           <MovieInfoRow label="Country" value={movie?.Country} />
+
           <MovieInfoRow label="Released" value={movie?.Released} />
+
           <MovieInfoRow label="IMDb Rating" value={movie?.imdbRating} />
+
           <MovieInfoRow label="IMDb Votes" value={movie?.imdbVotes} />
+
           <MovieInfoRow label="Awards" value={movie?.Awards} />
         </View>
 
@@ -154,6 +174,7 @@ const DetailsScreen = () => {
                   >
                     {r.Value}
                   </Text>
+
                   <Text
                     style={{
                       color: "#94a3b8",
@@ -184,14 +205,13 @@ const DetailsScreen = () => {
           onPress={async () => {
             if (!movie) return;
 
-            const nowSaved = await toggleSave({
-              imdbID: movie?.imdbID,
-              Title: movie?.Title,
-              Poster: movie?.Poster,
-              Year: movie?.Year,
+            await toggleSaveMovie({
+              imdbID: movie.imdbID,
+              Title: movie.Title,
+              Poster: movie.Poster,
+              Year: movie.Year,
+              Type: movie.Type || "movie",
             });
-
-            setSaved(nowSaved);
           }}
         >
           <Text
@@ -217,31 +237,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.backgroundColor,
   },
+
   moviePoster: {
     width: "100%",
     height: vs(350),
     resizeMode: "cover",
     borderRadius: 12,
   },
+
   movieTitle: {
     fontSize: s(22),
     color: colors.textColor,
     fontWeight: "800",
   },
+
   movieInfoContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
     gap: 8,
   },
+
   movieRatingContainer: {
     flexDirection: "row",
     alignItems: "center",
     marginTop: vs(4),
   },
+
   movieRatingText: {
     color: "gold",
     fontWeight: "700",
   },
+
   noRatingsText: {
     textAlign: "left",
     color: "#94a3b8",
