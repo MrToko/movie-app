c3e718753ce8c9bd7e3f5d63ef8f9918b938255a
TheTokoo <emirhan.tok@stu.fsm.edu.tr>
2026-05-15 14:56:51 +0300

    refactor: separate api services and models
    Refactored API structure by separating services and models into dedicated folders using TypeScript interfaces.

---
+++ src/api/services/movie.service.ts
@@ -0,0 +1,47 @@
+const API_BASE = "https://www.omdbapi.com/";
+const API_KEY = "9b1460f6";
+
+export async function searchMovies(
+  query: string,
+  page: number = 1
+) {
+  try {
+    const url = `${API_BASE}?apikey=${API_KEY}&s=${encodeURIComponent(
+      query
+    )}&page=${page}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("❌ Failed to fetch movie ❌");
+
+    if (error instanceof Error) {
+      console.log("Failed detail: ", error.message);
+    } else {
+      console.log("Unknown error: ", error);
+    }
+  }
+}
+
+export async function getMovieDetails(imdbID: string) {
+  try {
+    const url = `${API_BASE}?apikey=${API_KEY}&i=${encodeURIComponent(
+      imdbID
+    )}&plot=full`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("❌ Failed to fetch movie details ❌");
+
+    if (error instanceof Error) {
+      console.log("Failed detail: ", error.message);
+    } else {
+      console.log("Unknown error: ", error);
+    }
+  }
+}
